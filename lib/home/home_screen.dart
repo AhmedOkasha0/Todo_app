57c52ba4f6fdb79189cd146200146d3f9417f186
AhmedOkasha0 <119317835+AhmedOkasha0@users.noreply.github.com>
2023-03-19 13:23:15 +0200

    finished app

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,16 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:todo/theme_app.dart';
 
-class HomeScreen extends StatelessWidget {
-  static const String routeName='homeScreen';
+class HomeScreen extends StatefulWidget {
+  static const String routeName = 'homeScreen';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
-        title:Text('To Do List',
+        title: Text(
+          'To Do List',
+          style: Theme.of(context).textTheme.subtitle1,
+        ),
       ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 8,
+        child: BottomNavigationBar(
+          currentIndex: selectedIndex,
+          onTap: (index) {
+            selectedIndex = index;
+            setState(() {
+              print(index);
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage(
+                    'assets/images/Icon_settings.png',
+                  ),
+                ),
+                label: 'settings'),
+            BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage(
+                    'assets/images/Icon awesome-list.png',
+                  ),
+                ),
+                label: 'Menu'),
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            width: 4,
+            color: Colors.white,
+          ),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        onPressed: () {
 
+        },
+        child: Icon(
+          Icons.add,
+          size: 30,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
+
+
 }
